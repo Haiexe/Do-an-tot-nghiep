ef679fff865caa1d0dd2de844c48b750eb2488b6
Haiexe <tienhaipham06012002@gmail.com>
2024-10-19 13:30:57 +0700

    init project, FE - login-, logout

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import { Home, Login } from "./containers/Public";
+import { path } from "./utils/constant";
+
+function App() {
+  return (
+    <div className="h-screen w-screen bg-primary">
+      <Routes>
+        <Route path={path.HOME} element={<Home></Home>}>
+          <Route path={path.LOGIN} element={<Login></Login>}></Route>
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
